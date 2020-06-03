c8053fc964c09bf00ceefec6d425e182db4c63e9
bagama7777 <bakhronov@gmail.com>
2020-06-04 00:53:22 +0500

    added new items page

--- app/js/main.js
+++ app/js/main.js
@@ -2,7 +2,7 @@ $(function () {
 
 
     $(".rate-star").rateYo({
-        rating: 5,
+        rating: 4.5,
         starWidth: "13px",
         readOnly: true
     });
@@ -12,6 +12,26 @@ $(function () {
         arrows: false,
         slidesToShow: 4,
         slidesToScroll: 4
+    });
+
+    $(".js-range-slider").ionRangeSlider({
+        type: "double",
+        min: 0,
+        max: 1000,
+        from: 0,
+        to: 600,
+        prefix: "$"
+    });
+
+    $('.icon-th-list').on('click',function(){
+        $('.product__item').addClass('list');
+        $('.icon-th-list').addClass('active');
+        $('.icon-th-large').removeClass('active');
+    });
+    $('.icon-th-large').on('click',function(){
+        $('.product__item').removeClass('list');
+        $('.icon-th-large').addClass('active');
+        $('.icon-th-list').removeClass('active');
     });
 
 
